3601fb66fcd3b5540ab610b92bb4725cc22c6e9c
AntonioChacondelaPrada <a.chacondelaprada@gmail.com>
2026-04-04 14:04:13 +0200

    creating tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,23 @@
+import re
+
+class SimpleTokenizer2:
+    def __init__(self, vocab):
+        #stores the vocabulary as a class attribute for access in the encode and decode methods
+        self.str_to_int =  vocab
+        #Crates an inverse vocabulary that maps token IDs back to the original text tokens
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    # encode function- Processes input text into token IDs
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    # decode function - Converts token IDs back into text
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        #Removes spaces before the specified punctiation
+        text = re.sub(r'\s+([,.?!"()\'])', r'\l', text)
+        return text
--- tokenizing_text.py
+++ tokenizing_text.py
@@ -1,4 +1,7 @@
+# TOKENIZING TEXT
+
 import urllib.request
+import re
 url = ("https://raw.githubusercontent.com/rasbt/"
        "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
        "the-verdict.txt")
@@ -7,5 +10,22 @@ urllib.request.urlretrieve(url, filepath)
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
        raw_text = f.read()
-print("Total number of character: ", len(raw_text))
-print(raw_text [:99])
+# print("Total number of character: ", len(raw_text))
+# print(raw_text [:99])
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+# print(len(preprocessed))
+# print(preprocessed[:30])
+
+#CONVERTING TOKENS INTO IDs
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+vocab = {token: integer for integer, token in enumerate(all_words)}
+
+# for i, item in enumerate(vocab.items()):
+#        print(item)
+#        if i >= 50:
+#               break
+
